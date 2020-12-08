c4b7b89f7cdd2d8e2a01ada9eababe448589e8e4
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-12-08 18:18:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.content.startswith("hi"):
+        	await client.send_message(message.channel, "HI")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
