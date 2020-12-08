d5e07d437a90b51fc712386bd3130905d07a1629
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-12-08 17:47:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import discord
 import os
 
-ROLE = "NoVerify"
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	print(client.user.id)
 	print("ready")
 	await client.change_presence(game=discord.Game(name='', type=1))
 
 @client.event
-async def on_member_join(member):
-	role = get(member.guild.roles, name=ROLE)
-	await member.add_roles(role)
-	channel = discord.utils.get(member.guild.channels, name='환영합니다.')
-	await channel.send(f'hey there, {member.mention}, welcome to my server!')
+async def on_message(message):
+	if message.content.startswith("hi"):
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
