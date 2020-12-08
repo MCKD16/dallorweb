5d08f2e3fd97c7c5ba4d9e6d3155b505b6b7fad6
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-12-08 18:21:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
 	print("ready")
-	await client.change_presence(game=discord.Game(name='', type=1))
 
 @client.event
 async def on_message(message):
